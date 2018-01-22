9bc4d08253b3b7a73f810ea3a66f6e6d36ff8d50
Fernando Balieiro <fernando.balieiro@movile.com>
2018-01-22 16:38:27 -0200

    Section 12 Assignment.

---
+++ vue/section-12-assignment/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-myonclick:click="logSomething">Click Me!</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    directives: {
+        myonclick: {
+            bind(el, binding) {
+                // el.onclick = function() {
+                //     binding.value();
+                // };
+                const type = binding.arg;
+                const fn = binding.value;
+                el.addEventListener(type, fn);
+            }
+        }
+    },
+    methods: {
+        logSomething() {
+            console.log("Something");
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
