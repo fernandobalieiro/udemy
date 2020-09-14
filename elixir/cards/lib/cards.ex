febedd209eb63df0e32b6413eb22b66de4278917
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-09-14 22:36:28 +0100

    Methods documentation.

--- elixir/cards/lib/cards.ex
+++ elixir/cards/lib/cards.ex
@@ -3,6 +3,9 @@ defmodule Cards do
     Provides methods for creating and handling a deck of cards
   """
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   @spec create_deck :: [<<_::24, _::_*16>>, ...]
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six"]
@@ -23,6 +26,17 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples:
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
     # { hand, _rest_of_deck } = Enum.split(deck, hand_size)
