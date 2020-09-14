e3f3496e7547be91e583cd2e2d527dd003ab7cf3
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-09-14 22:11:04 +0100

    Initial load solution.

--- elixir/cards/lib/cards.ex
+++ elixir/cards/lib/cards.ex
@@ -1,16 +1,12 @@
 defmodule Cards do
   @spec create_deck :: [<<_::24, _::_*16>>, ...]
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    cards = for value <- values do
-      for suit <- suits do
+    for suit <- suits, value <- values do
         "#{value} of #{suit}"
-      end
     end
-
-    List.flatten(cards)
   end
 
   @spec shuffle(any) :: [any]
@@ -21,5 +17,25 @@ defmodule Cards do
   @spec contains?(any, any) :: boolean
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+    # { hand, _ } = Enum.split(deck, hand_size)
+    # hand
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist"
+    end
   end
 end
