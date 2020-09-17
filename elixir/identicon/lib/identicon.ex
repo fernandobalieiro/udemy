dc6375f0b29ccb73d28559c080289958431d644b
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-09-17 12:50:41 +0100

    Added color to Image struct.

--- elixir/identicon/lib/identicon.ex
+++ elixir/identicon/lib/identicon.ex
@@ -11,15 +11,14 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
-  end
-
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
