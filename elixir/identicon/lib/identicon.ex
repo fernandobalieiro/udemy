f6daffd0e00e79fcce885bc5d929bacb5ecff3b8
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-09-23 09:05:50 +0100

    Filtering odd squares.

--- elixir/identicon/lib/identicon.ex
+++ elixir/identicon/lib/identicon.ex
@@ -10,6 +10,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def hash_input(input) do
@@ -40,5 +41,11 @@ defmodule Identicon do
     row ++ [second, first]
   end
 
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
 
+    %Identicon.Image{image | grid: grid}
+  end
 end
