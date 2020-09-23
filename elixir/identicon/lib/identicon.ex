045ada6efaf06f4a165d51a93d867334b25a90d4
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-09-23 08:56:50 +0100

    Added grid structure.

--- elixir/identicon/lib/identicon.ex
+++ elixir/identicon/lib/identicon.ex
@@ -23,15 +23,22 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
-  def build_grid(%Identicon.Image{hex: hex}) do
-    hex
-    # FIXME Check best alternative to avoid deprecated call
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      # FIXME Check best alternative to avoid deprecated call
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
   end
+
+
 end
