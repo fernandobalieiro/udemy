5afd497557bdcdba2d45da2d1e0449c74ef97d62
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-11-30 16:46:38 +0000

    Flyway usage Example.

---
+++ java/quarkus/migration-flyway/src/main/resources/db/migration/V1.0.0__initial_description.sql
@@ -0,0 +1,7 @@
+create table user(id int, name text);
+
+insert into user values(1, 'Alfred');
+insert into user values(2, 'Batman');
+insert into user values(3, 'Robin');
+insert into user values(4, 'Superman');
+
---
+++ java/quarkus/migration-flyway/src/main/resources/db/migration/V1.0.1__Update_users.sql
@@ -0,0 +1,2 @@
+insert into user values(5, 'Aquaman');
+insert into user values(6, 'Green Lantern');
