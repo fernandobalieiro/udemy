dc9f83554adb0c59e5640aa9f721060e584dcc81
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-12-05 18:51:14 +0000

    JPA Security Example.

---
+++ java/quarkus/security-jpa/src/main/java/org/acme/data/User.java
@@ -0,0 +1,51 @@
+package org.acme.data;
+
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+@UserDefinition
+public class User extends PanacheEntity {
+    @Username
+    private String username;
+
+    @Password
+    private String password;
+
+    @Roles
+    private String roles;
+
+    public User() {}
+
+    public User(String username, String password, String roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public static User add(final String username, final String password, final String roles) {
+        final var user = new User(username, BcryptUtil.bcryptHash(password), roles);
+        user.persist();
+        return user;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+}
